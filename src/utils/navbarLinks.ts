29bfb2670055516782662f3e99edfabe4bb66f93
Luís Lança <luis_lanca@outlook.com>
2023-04-04 16:07:21 -0300

    Update navbarLinks.ts

--- src/utils/navbarLinks.ts
+++ src/utils/navbarLinks.ts
@@ -6,11 +6,7 @@ const LINKS = [
   {
     url: "timeline",
     label: "work xp",
-  },
-  {
-    url: "https://projects.luislanca.com/",
-    label: "projects",
-  },
+  }
 ];
 
 export default LINKS
