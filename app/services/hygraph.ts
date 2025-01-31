3a5b1398780a87569b2ebe3736080418d81af96f
Luís Lança <luis_lanca@outlook.com>
2025-01-31 11:34:01 -0300

    chore: changing to SSR fetch instead SSG

---
+++ app/services/hygraph.ts
@@ -0,0 +1,50 @@
+import { GraphQLClient } from "graphql-request";
+
+const HYGRAPH_API = process.env.HYGRAPH_API!;
+export const hygraph = new GraphQLClient(HYGRAPH_API);
+
+export const getPlaces = async () => {
+  const query = `
+  {
+    places {
+      id
+      slug
+      title
+      location {
+        latitude
+        longitude
+      }
+    }
+  }
+`;
+
+  return await hygraph.request(query);
+};
+
+export const getPlaceBySlug = async (slug: string) => {
+  const query = `
+    query GetPlaceBySlug($slug: String!) {
+      place(where: { slug: $slug }) {
+        id
+        slug
+        title
+        subtitle
+        content {
+          html
+        }
+        location {
+          latitude
+          longitude
+        }
+        gallery {
+          url
+          width
+          height
+        }
+      }
+    }
+  `;
+
+  const variables = { slug };
+  return await hygraph.request(query, variables);
+};
--- app/trips/[slug]/page.tsx
+++ app/trips/[slug]/page.tsx
@@ -1,50 +1,10 @@
-import { notFound } from "next/navigation";
-import { getPlaceBySlug, getPlaces } from "@/graphql/client";
+import { getPlaceBySlug } from "@/services/hygraph";
 import PlaceComponent from "@/components/Place";
 
-
-export async function generateStaticParams() {
-  try {
-    const result = await getPlaces();
-
-    if (!result || !result.places || !Array.isArray(result.places)) {
-      console.error("Resposta da API inválida:", result);
-      return [];
-    }
-
-    return result.places.map((page: { slug: string }) => ({
-      slug: page.slug,
-    }));
-  } catch (error) {
-    console.error("Erro ao obter páginas:", error);
-    return [];
-  }
-}
-
-// Buscar dados do lugar
-async function getPlaceData(slug: string) {
-  try {
-    const result = await getPlaceBySlug(slug);
-
-    if (!result) {
-      return null;
-    }
-
-    return result;
-  } catch (error) {
-    console.error("Erro ao obter dados da página:", error);
-    return null;
-  }
-}
-
 export default async function Place({ params }: { params: { slug: string } }) {
-  const { slug } = await params;
+  const { slug } = params;
 
-  const place = await getPlaceData(slug);
-
-  if (!place) {
-    notFound();
-  }
+  const { place } = await getPlaceBySlug(slug);
 
   return <PlaceComponent place={place} />;
 }
