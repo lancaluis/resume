23d224a9ce1bb5b90a3f619fcd201385cf16dabd
Luís Lança <luis_lanca@outlook.com>
2025-01-30 22:57:19 -0300

    chore: changing spacing inside home

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { About, Hero, Skills } from "@/components";
 
 const Home = () => {
   return (
-    <div className="space-y-12">
+    <div className="space-y-14">
       <Hero />
       <About />
       <Skills />
