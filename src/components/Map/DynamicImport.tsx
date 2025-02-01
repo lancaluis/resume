6ad4debd101de9d64baf558dc5981c6953da3860
Luís Lança <luis_lanca@outlook.com>
2025-02-01 19:11:14 -0300

    chore: adding loading while slug page is creating

--- src/components/Map/DynamicImport.tsx
+++ src/components/Map/DynamicImport.tsx
@@ -3,17 +3,14 @@
 import dynamic from "next/dynamic";
 import { Places } from "../../types";
 
-const Map = dynamic(
-  () => import(".").then((component) => component.Map),
-  {
-    ssr: false,
-    loading: () => (
-      <div className="flex justify-center items-center">
-        <span className="loading loading-spinner loading-lg"></span>
-      </div>
-    ),
-  }
-);
+const Map = dynamic(() => import(".").then((component) => component.Map), {
+  ssr: false,
+  loading: () => (
+    <div className="flex items-center justify-center h-screen">
+      <span className="loading loading-spinner loading-lg"></span>
+    </div>
+  ),
+});
 
 const DynamicMap = ({ places }: Places) => {
   return <Map places={places} />;
